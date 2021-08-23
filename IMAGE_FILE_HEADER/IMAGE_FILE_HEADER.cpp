b3957bdf480dc0a307b5b7d26976ada44c56257e
kgyeongseong <83118180+kgyeongseong@users.noreply.github.com>
2021-08-23 03:50:14 -0700

    Add files via upload

---
+++ IMAGE_FILE_HEADER/IMAGE_FILE_HEADER.cpp
@@ -0,0 +1,34 @@
+#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    HANDLE hFile = CreateFileA("C:\\Users\\root\\Desktop\\Reversing\\PEview.exe",
+        GENERIC_READ, FILE_SHARE_READ, 0, OPEN_EXISTING, 0, 0);
+    if (hFile == INVALID_HANDLE_VALUE) {
+        cout << "CreateFileA error 0X%X" << GetLastError() << endl;
+        return 0;
+    }
+
+    HANDLE hFileMappingObject = CreateFileMappingA(hFile, 0, PAGE_READONLY, 0, 0, 0);
+    if (!hFileMappingObject) {
+        cout << "CreateFileMappingA error 0X%X" << GetLastError() << endl;
+        return 0;
+    }
+
+    char* pBaseOfFile = (char*)MapViewOfFile(hFileMappingObject, FILE_MAP_READ, 0, 0, 0);
+    if (!pBaseOfFile) {
+        cout << "MapViewOfFile error 0X%X" << GetLastError() << endl;
+        return 0;
+    }
+
+    PIMAGE_DOS_HEADER pImage_Dos_Header = (PIMAGE_DOS_HEADER)pBaseOfFile;
+
+    // BaseOfFile + (PIMAGE_DOS_HEADER->e_lfanew) + 4 == PIMAGE_FILE_HEADER
+    PIMAGE_FILE_HEADER pImage_File_Header = (PIMAGE_FILE_HEADER)((LONG)pImage_Dos_Header + pImage_Dos_Header->e_lfanew + 4);
+    cout << uppercase << hex << pImage_File_Header->Machine << endl;
+
+    return 0;
+}
